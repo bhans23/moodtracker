fb19aae80d9e7016570b1d526dc45e434ffe2b5f
Ben <bhans23@gmail.com>
2020-03-12 07:12:46 -0600

    Initializing react app

---
+++ src/shared/Tracker.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Tracker = () => {
+  return (
+    <>
+      <h1>Mood Tracker 5000!</h1>
+      <p>This is going to be the best mood tracker...maybe...perhaps</p>
+    </>
+  );
+};
+
+export default Tracker;
