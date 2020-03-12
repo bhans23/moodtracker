6a14286476cfeaf72afe94903ee44b28b4286687
Ben <bhans23@gmail.com>
2020-03-12 16:14:25 -0600

    added slider and values variables

--- src/shared/Tracker.js
+++ src/shared/Tracker.js
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import "../css/Tracker.css";
 
 const Tracker = () => {
+  const [moodValue, setMoodValue] = useState(0);
+  const [currentDate, setCurrentDate] = useState("");
+  const [currentTime, setcurrentTime] = useState("");
+
+  const handleChange = (e) => {
+    setMoodValue(e.target.value);
+  };
+
   return (
-    <>
+    <div className="tracker">
       <h1>Mood Tracker 5000!</h1>
       <p>This is going to be the best mood tracker...maybe...perhaps</p>
-    </>
+      <div className="slideContainer">
+        <h3>{moodValue}</h3>
+        <form>
+          <input
+            type="range"
+            min="1"
+            max="10"
+            value={moodValue}
+            className="slider"
+            onChange={handleChange}
+            id="myRange"
+          />
+          <button>submit</button>
+        </form>
+      </div>
+    </div>
   );
 };
 
