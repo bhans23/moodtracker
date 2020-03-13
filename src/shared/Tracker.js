750bfbe7b168047c2c28071aa782986dd0436edd
Ben <bhans23@gmail.com>
2020-03-13 13:23:38 -0600

    Testing moodList

--- src/shared/Tracker.js
+++ src/shared/Tracker.js
@@ -2,25 +2,28 @@ import React, { useState } from "react";
 import "../css/Tracker.css";
 
 const Tracker = () => {
-  const [moodValue, setMoodValue] = useState(0);
+  // const [moodValue, setMoodValue] = useState(0);
   const [moodList, setMoodList] = useState([]);
   // const [currentDate, setCurrentDate] = useState("Today's Date");
   // const [currentTime, setcurrentTime] = useState("Current Time");
 
   const handleSubmit = e => {
     e.prevenDefault();
-    setMoodList({
-      date: new Date().toLocaleDateString(),
-      time: new Date().toLocaleTimeString(),
-      mood: moodValue
-    });
+    setMoodList(moodList => [
+      ...moodList,
+      {
+        date: new Date().toLocaleDateString(),
+        time: new Date().toLocaleTimeString(),
+        
+      }
+    ]);
     console.log(moodList.date);
   };
 
   const handleChange = e => {
     // const dayTime = new Date();
-
-    setMoodValue(e.target.value);
+      setMoodList(moodList => [...moodList,{mood: e.target.value}])
+    // setMoodValue(e.target.value);
     // setCurrentDate(dayTime.toLocaleDateString());
     // setcurrentTime(dayTime.toLocaleTimeString());
   };
@@ -30,14 +33,14 @@ const Tracker = () => {
       <h1>Mood Tracker 5000!</h1>
       <p>This is going to be the best mood tracker...maybe...perhaps</p>
       <div className="slideContainer">
-        <h3>{moodValue}</h3>
+        <h3>{moodList[0].mood}</h3>
 
         <form submit={handleSubmit}>
           <input
             type="range"
             min="1"
             max="10"
-            value={moodValue}
+            value={moodList[0].mood}
             className="slider"
             onChange={handleChange}
             id="myRange"
@@ -45,6 +48,7 @@ const Tracker = () => {
           <button type="submit">submit</button>
         </form>
       </div>
+      {console.log(moodList[0])}
     </div>
   );
 };
