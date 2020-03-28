90b223d0531bebad8de0b2e036b5e3ccf3117632
Ben <bhans23@gmail.com>
2020-03-28 17:43:09 -0600

    Fixed that only one moodItem would get toggled on click

--- src/components/MoodItem.js
+++ src/components/MoodItem.js
@@ -1,40 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
 import MoodItemForm from "./MoodItemForm";
 
-const MoodItem = ({ moodObject ,  handleSubmit }) => {
-  const [editToggle, setEditToggle] = useState(false);
+const MoodItem = ({ moodObject, handleSubmit }) => {
+  
   const { date, moods } = moodObject;
+
  
- 
-    if (editToggle === false) {
-      return (
-        <div>
-          <h1>Day: {date} </h1>
-          {moods.map(moodItem => (
-            <div>
-              Your Mood was: {moodItem.mood} at: {moodItem.time}
-              <button
-                onClick={toggle => toggle = setEditToggle(true)}
-              >
-                edit
-              </button>
-            </div>
-          ))}
-        </div>
-      );
-    } else {
-      return (
-        <div>
-          <h1>Day: {date} </h1>
-          {moods.map(moodItem => (
-            <div>
-              <MoodItemForm moodItem={moodItem} handleSubmit={handleSubmit} setEditTogle={setEditToggle} />
-            </div>
-          ))}
-        </div>
-      );
-    }
- 
+
+  return (
+    <div>
+      <h1>Day: {date} </h1>
+
+      {moods.map(moodItem => <MoodItemForm moodItem={moodItem}/>)}
+    </div>
+  );
 };
 
 export default MoodItem;
--- src/components/MoodItemForm.js
+++ src/components/MoodItemForm.js
@@ -1,32 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 
 const MoodItemForm = props => {
-  const { moodItem, handleSubmit, setEditToggle } = props;
+  const { moodItem, handleSubmit } = props;
+  const [editToggle, setEditToggle] = useState(false);
 
-  return (
-    <>
-      <form onSumbit={handleSubmit}>
-        <label for="mood">Your Mood was:</label>
-        <select id="mood">
-          <option value={moodItem.mood}>{moodItem.mood}</option>
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
-          <option value="6">6</option>
-          <option value="7">7</option>
-          <option value="8">8</option>
-          <option value="9">9</option>
-          <option value="10">10</option>
-        </select>
-        at: {moodItem.time}
-        <button onClick={toggle => (toggle = setEditToggle(false))} type="submit">
-          Submit
-        </button>
-      </form>
-    </>
-  );
+  if (editToggle === false) {
+    return (
+      <div>
+        Your Mood was: {moodItem.mood} at: {moodItem.time}
+        <button onClick={toggle => (toggle = setEditToggle(true))}>edit</button>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <>
+          <form onSumbit={handleSubmit}>
+            <label for="mood">Your Mood was:</label>
+            <select id="mood">
+              <option value={moodItem.mood}>{moodItem.mood}</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select>
+            at: {moodItem.time}
+            <button
+              onClick={toggle => (toggle = setEditToggle(false))}
+              type="submit"
+            >
+              Submit
+            </button>
+          </form>
+        </>
+        
+      </div>
+    );
+  }
 };
 
 export default MoodItemForm;
