dd4dc8bd33caac6e571858b21a027a6117cfeaae
Ben <bhans23@gmail.com>
2020-05-23 12:33:55 -0600

    Added post to exporess

--- moodExpress/app.js
+++ moodExpress/app.js
@@ -6,26 +6,38 @@ const port = 3001;
 
 const proxy = proxyMiddleware("http://localhost:3000");
 
-// [http://localhost:3001/api/moods]
-app.get("/api/moods", (request, res) => res.json(moodData()));
+let moodData = [
+  {
+    moods: [
+      { mood: 3, time: new Date("April 26, 2020 01:15:00") },
+      { mood: 6, time: new Date("April 26, 2020 03:15:00") },
+      { mood: 10, time: new Date("April 26, 2020 03:15:00") },
+    ],
+    date: "4/26/2020",
+  },
+];
 
-app.post("/api/moods", (req, res) => moodData(req));
+app.use(express.json());
+
+// [http://localhost:3001/api/moods]
+app.get("/api/moods", (req, res) => res.json(moodData));
+
+app.post("/api/moods", (req, res) => {
+  const data = req.data;
+  const moodDateObject = moodData.find(
+    (moodObject) => moodObject.date === data.date
+  );
+
+  if (moodDateObject) {
+    mooodDateObject.moods = [...moodDateObject.moods, ...data.moods];
+    return [...moodData];
+  } else {
+    return [(moodData = [...moodData, data])];
+  }
+});
 
 app.use("/", proxy);
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
 );
-
-const moodData = (req) =>
-  [
-    {
-      moods: [
-        { mood: 3, time: new Date("April 26, 2020 01:15:00") },
-        { mood: 6, time: new Date("April 26, 2020 03:15:00") },
-        { mood: 10, time: new Date("April 26, 2020 03:15:00") },
-      ],
-      date: "4/26/2020",
-    },
-  ].map((moodObject) => ({ ...moodObject, ...req }));
-
